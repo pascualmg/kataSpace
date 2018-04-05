e884d869f4eacc93b9154a097b3c51c3c7b28cdf
Pascual <pascual.munoz@pccomponentes.com>
2018-04-05 17:25:09 +0200

    example

--- kata.php
+++ kata.php
@@ -7,12 +7,19 @@
  */
 
 
+use Apigator\httpClient\ApiGator;
+
 require 'vendor/autoload.php';
 $uri_humans_in_space = "http://api.open-notify.org/astros.json";
-
 var_export(
     ((new ApiGator())
     ->setUri($uri_humans_in_space)
     ->getArrayResponse())['number']
 );
+file_put_contents(
+    "space.json",
+(new ApiGator($uri_humans_in_space))->getCurlResponse()
+);
 
+$json = file_get_contents("space.json");
+echo $json;
